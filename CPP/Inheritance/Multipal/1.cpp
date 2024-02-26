913822278fef86c94a1901f1495b5346dc71dbf6
divyeshdankhara34@gmail.com <divyeshdankhara34@gmail.com>
2024-02-26 10:00:16 +0530

    26-02-2024

---
+++ CPP/Inheritance/Multipal/1.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+
+class base1
+{
+    protected:
+    int a1;
+
+    public:
+    void setdata1(int x1)
+    {
+        a1 = x1;
+    }
+};
+class base2
+{
+    protected:
+    int a2;
+
+    public:
+    void setdata2(int x2)
+    {
+        a2 = x2;
+    }
+};
+
+class derived:public base1,public base2
+{
+    public:
+    void show()
+    {
+        cout << "The value of 1 :-> " << a1 << endl;
+        cout << "The value of 2 :-> " << a2 << endl;
+        cout << "The sum of :-> " << a1+a2;
+    }
+};
+int main()
+{
+    derived obj;
+    obj.setdata1(10);
+    obj.setdata2(20);
+    obj.show();
+
+    return 0;
+}
